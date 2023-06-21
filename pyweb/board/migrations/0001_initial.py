7139a87ceba09f2574ce6465bad45949b95dba0b
kiyongee2 <kiyongee2@gmail.com>
2023-06-21 17:33:02 +0900

    pyweb common signup

--- pyweb/board/migrations/0001_initial.py
+++ pyweb/board/migrations/0001_initial.py
@@ -13,6 +13,7 @@ class Migration(migrations.Migration):
     operations = [
         migrations.CreateModel(
             name='Question',
+
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('subject', models.CharField(max_length=200)),
