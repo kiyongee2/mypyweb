3bde036e0df56a0573f9d8bc8e66f27047d8aaa2
kiyongee2 <kiyongee2@gmail.com>
2023-06-29 11:53:39 +0900

    debug setting

--- pyweb/config/settings.py
+++ pyweb/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ypq=v*5%zheco2v*s#oa51y=_0lom68^qv4%%ks6opib*^=$i)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
